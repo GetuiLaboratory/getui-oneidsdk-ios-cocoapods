7f7f107ba98ba9b5a051c4cf584ff191cecfaa86
linwanting <linwt@getui.com>
2024-07-29 11:13:05 +0800

    update: 更新1.0.4.0版本

--- OneID.xcframework/ios-arm64_x86_64-maccatalyst/OneID.framework/Headers/OneID.h
+++ OneID.xcframework/ios-arm64_x86_64-maccatalyst/OneID.framework/Headers/OneID.h
@@ -3,7 +3,7 @@
 //  OneID
 //
 //  Created by ak on 2024/2/4.
-//  OneID-Version: 1.0.1.0
+//  OneID-Version: 1.0.4.0
 
 #import <Foundation/Foundation.h>
 
@@ -30,8 +30,10 @@ NS_ASSUME_NONNULL_BEGIN
 /// 获取缓存中的OneID
 + (NSDictionary*)oneIDFetchInfo;
 
-/// 查询Lbs数据，通过回调- (void)OneIDQueryLbs:(NSDictionary *)info error:(nullable NSError *)error; 接收数据
-+ (void)queryLbs;
+/// 查询Lbs数据（获取时间间隔内的gps数据，如果超出间隔则实时获取gps数据）
+/// - Parameter gpsInterval: gps有效期设置，可以设置范围为-1 或 [0, 600]，单位秒。0：实时获取gps数据。 -1：使用系统缓存的gps数据
+/// - Parameter gpsTimeout: 超时设置，可以设置范围为[10, 60]，单位秒,  建议设置20
++ (void)queryLbs:(NSTimeInterval)gpsInterval gpsTimeout:(NSTimeInterval)gpsTimeout;
 
 + (NSString*)sdkVersion;
 
